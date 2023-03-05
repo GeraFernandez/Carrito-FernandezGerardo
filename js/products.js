413c36a55a5c20138df01415b2e25acb0639816a
Gerardo Fernandez <geraf9@gmail.com>
2023-03-04 23:20:56 -0300

    first commit

---
+++ js/products.js
@@ -0,0 +1,35 @@
+const productos = [
+  {
+    id: 1,
+    nombre: "Gorra",
+    precio: 500,
+    img:
+      "https://assets.stickpng.com/thumbs/580b585b2edbce24c47b2ad2.png",
+    cantidad: 1,
+  },
+  
+  {
+    id: 2,
+    nombre: "Remera",
+    precio: 7500,
+    img:
+      "https://assets.stickpng.com/images/580b57fbd9996e24bc43bf77.png",
+    cantidad: 1,
+  },
+  {
+    id: 3,
+    nombre: "Pantalon",
+    precio: 15000,
+    img:
+      "https://assets.stickpng.com/thumbs/580b57fbd9996e24bc43bf36.png",
+    cantidad: 1,
+  },
+  {
+    id: 4,
+    nombre: "Zapatillas",
+    precio: 25000,
+    img:
+      "https://assets.stickpng.com/thumbs/5ba65b27bede2105e7aaeede.png",
+    cantidad: 1,
+  },
+];
